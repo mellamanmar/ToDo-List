eefe2087ae89db6bddecefcb68498985cdeab1ad
mellamanmar <marimelissamc@gmail.com>
2023-07-06 11:42:51 -0500

    Created the missing CRUD for status model (get. get for id and update)

--- controllers/status.js
+++ controllers/status.js
@@ -1,19 +1,68 @@
 const Status = require('../models/status')
 
 const controllerStatus = {
-  create: async (req,res) => {
+  // create: async (req,res) => {
+  //   try {
+  //     const status = req.body.status
+  //     await Status.create ({
+  //       status : status
+  //     })
+  //     res.json({msg:'Created'})
+
+  //   } catch (error) {
+  //     return res.status(500).json ({msg:error.message})
+  //   }
+  // }
+
+  get: async (req,res) => {
     try {
+      const status = await Status.find({})
+
+      res.json(
+        status,
+        )
+    } catch (error) {
+      return res.status(500).json({msg:error.message})
+    }
+  },
+
+  getById: async (req,res) => {
+    try {
+      const {id} = req.params
+      const status = await Status.findById(id)
+
+      res.json(
+        status,
+        )
+    } catch (error) {
+      return res.status(500).json({msg:error.message})
+    }
+  },
+
+  update: async (req,res) => {
+    try {
+      const {id} = req.params
       const status = req.body.status
-      await Status.create ({
+      await Status.findByIdAndUpdate(id, {
         status : status
       })
-      res.json({msg:'Created'})
 
+      res.json({msg:'Updated'})
     } catch (error) {
-      return res.status(500).json ({msg:error.message})
+      return res.status(500).json({msg:error.message})
     }
-  }
+  },
 
+  // delete: async (req,res)=>{
+  //   try {
+  //       const {id} = req.params
+  //       await Status.findByIdAndDelete(id)
+  //       res.json({msg:'Deleted'})
+  //   } catch (err) {
+  //       console.error(err)
+  //       return res.status(500).json({msg:err.message})
+  //   }
+  // },
 }
 
 module.exports = controllerStatus
--- routes/status.js
+++ routes/status.js
@@ -2,6 +2,10 @@ const express = require('express')
 const controllerStatus = require('../controllers/status')
 const router = express.Router()
 
-router.post ('/create', controllerStatus.create)
+// router.post ('/create', controllerStatus.create)
+router.get ('/', controllerStatus.get)
+router.get ('/:id', controllerStatus.getById)
+router.patch ('/update/:id', controllerStatus.update)
+// router.delete ('/delete/:id', controllerStatus.delete)
 
 module.exports = router
